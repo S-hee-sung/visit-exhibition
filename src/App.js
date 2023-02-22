181eba0e84f44379b6639098c4f8aaec157a26d1
avery.Chae <chaehj91@gmail.com>
2023-02-22 17:01:33 +0900

    feature/main01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import MainPage from "./components/mainPage";
+// import Youtube from "./components/youtube";
 
 const GlobalStyle = createGlobalStyle`
+  ${reset};
+
   body: {
     box-sizing: border-box;
+    font-family: 'Nanum Gothic', sans-serif;
   }
-
+  * {
+    box-sizing: inherit;
+  }
   .cursor-pointer {
     cursor: pointer;
   }
@@ -15,7 +23,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-    
+      <MainPage/>
     </>
   );
 }

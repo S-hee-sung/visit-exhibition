faf06b840fba8e2aa23de30b5302fb2278a7113f
avery.Chae <chaehj91@gmail.com>
2023-02-22 14:11:40 +0900

    feature/map (branch 연습)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
-import LoginForm from "./components/login/LoginForm";
-import LoginTemplate from "./components/login/LoginTemplate";
 
 const GlobalStyle = createGlobalStyle`
   body: {

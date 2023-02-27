13486249b6dffff5c1e806ab2b8c11bcbd1b4772
kws04181 <kimsungwon57@naver.com>
2023-02-27 18:20:00 +0900

    feature/헤더,푸터,로그인-보완

---
+++ src/pages/FooterDropDown.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const familySite = [
+  {
+    id: 1,
+    text: 'famliy site1'
+  },
+  {
+    id: 2,
+    text: 'famliy site2'
+  },
+  {
+    id: 3,
+    text: 'famliy site3'
+  },
+  {
+    id: 4,
+    text: 'famliy site4'
+  },
+]
+
+function FooterDropDown(props) {
+
+  return (
+    <div>
+      {familySite.map((family) => {
+        return <li key={family.key} style={{ paddingTop: '10px' }}>{family.text}</li>
+      })}
+    </div>
+  );
+}
+
+export default FooterDropDown;

cd15bc88a7496f0538ac8c8ad1b4a013aaa6062f
hwajin-Chae <chaehj91@gmail.com>
2023-03-01 21:34:21 +0900

    feature/shop_03
    1. table HTML

--- src/components/shop(main04)/Cart.js
+++ src/components/shop(main04)/Cart.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { Button, Table } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 
-function Cart() {
-  return (
-    <div>
+const CartWrapper = styled.div`
+.inner {
+  max-width: 1200px;
+  margin: 0 auto;
+}`;
 
-    </div>
-  )
+function Cart(props) {
+  <>
+    <div>안녕</div>
+    <table>
+      <tr>
+        <th>No</th>
+        <th>상품명</th>
+        <th>가격</th>
+        <th>수량</th>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </>
+
 }
+
 
 export default Cart;

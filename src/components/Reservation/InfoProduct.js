151a9dedaae3ec0d3d74cc13a88cfd6a4647524c
S-hee-sung <dlsglrhdwjs@naver.com>
2023-03-03 19:39:13 +0900

    main-오류 수정

--- src/components/Reservation/InfoProduct.js
+++ src/components/Reservation/InfoProduct.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import DateCalender from './DateCalender';
 import MiniCalendar from './MiniCalender';
 
 const InfoArea = styled.div`

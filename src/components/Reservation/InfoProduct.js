0102050c1ec2995d0d09d4fbebd8a4a57c5b47f9
kimseongwon <kimsungwon57@naver.com>
2023-03-05 15:25:21 +0900

    feature/왓더F

--- src/components/Reservation/InfoProduct.js
+++ src/components/Reservation/InfoProduct.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MiniCalendar from './MiniCalender';
+import Test from './Test';
 
 const InfoArea = styled.div`
     flex: 6;
@@ -11,48 +12,76 @@ const InfoArea = styled.div`
     .infoProductArea1{
       height: 100%;
       display: flex;
-      gap: 30px;
       white-space: nowrap;
-      ul{
+      .infoArea{
+        width: 100%;
+        height: 100%;
         display: flex;
         flex-direction: column;
-        font-weight: bold;
-        font-size: 18px;
-        li{
-          margin: 20px 0;
-          &li:last-child{
-            margin: 0;
-          }
+        .categoryArea{
+          flex: 2;
+          background: blue;
         }
-        .focus{
-          font-weight: bold;
-          color: #ed4037;
+        .infoList{
+          flex: 1;
+          gap: 20px;
+          display: flex;
+          ul{
+            display: flex;
+            flex-direction: column;
+            font-weight: bold;
+            font-size: 18px;
+            li{
+              margin: 20px 0;
+              &li:last-child{
+                margin: 0;
+              }
+            }
+            .focus{
+              font-weight: bold;
+              color: #ed4037;
+            }
+          }
         }
       }
       .calChoice{
         width: 100%;
         margin-left: 80px;
+        li{
+          font-weight: bold;
+          font-size: 18px;
+        }
       }
     }
 `;
 
 
 function InfoProduct(props) {
+  const { contents } = props;
+
+
+  console.log(contents.content);
+
   return (
     <InfoArea>
       <div className='infoProductArea1'>
-        <ul>
-          <li>등급</li>
-          <li>관람시간</li>
-          <li>작가</li>
-          <li>가격</li>
-        </ul>
-        <ul style={{ fontWeight: "500", color: "#333" }}>
-          <li>전체이용가능</li>
-          <li>총 예상60분</li>
-          <li>김효선</li>
-          <li>입장 <span className='focus'>12,000</span>원</li>
-        </ul>
+        <div className='infoArea'>
+          <div className='categoryArea'>
+            <Test/>
+          </div>
+          <div className='infoList'>
+            <ul>
+              <li>등급</li>
+              <li>작가</li>
+              <li>가격</li>
+            </ul>
+            <ul style={{ fontWeight: "500", color: "#333" }}>
+              <li>전체이용가능</li>
+              <li>{contents.content}</li>
+              <li>입장 <span className='focus'>12,000</span>원</li>
+            </ul>
+          </div>
+        </div>
         <div className='calChoice'>
           <ul>
             <li>관람 날짜 선택</li>
--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -27,10 +27,38 @@ const ContentWrap = styled.div`
     background: yellow;
     padding: 20px;
   }
-
 `;
 
-function Reservation(props) {
+const contents = [
+  {
+    imageSrc: "https://www.drawingroom.kr/exhibition/current",
+    content: "슬픔은 파도처럼 밀려와",
+    location: "[drawingRoom]",
+    address: "서울 종로구 자하문로7길 68-4 2층"
+  },
+  {
+    imageSrc: "./images/slide01-2.jpg",
+    content: "NEW WAVE",
+    location: "[스페이스 사직]",
+    address: "서울 종로구 경희궁3가길 8-4"
+  },
+  {
+    imageSrc: "./images/slide01-3.jpg",
+    content: "이현정초대전",
+    location: "[정문규 미술관]",
+    address: "서울 종로구 성균관로5길 55-3"
+  },
+  {
+    imageSrc: "./images/slide01-4.jpg",
+    content: "최민솔개인전",
+    location: "[갤러리 도스]",
+    address: "서울 종로구 삼청로7길 37 갤러리도스"
+  },
+];
+
+
+function Reservation() {
+
   return (
     <ReservationWrap>
       <div className='inner'>
@@ -42,7 +70,7 @@ function Reservation(props) {
             })} */}
             <img src={data[1].src} width='100%' />
           </div>
-          <InfoProduct />
+          <InfoProduct testname="김성원" contents={contents}/>
         </ContentWrap>
 
       </div >
